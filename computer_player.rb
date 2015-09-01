9cbfa785927746159eaa9a9f76c22fd51e423fd7
Faye Keegan <faye.keegan@gmail.com>
2015-09-01 11:29:01 -0700

    refactor computer player code

--- computer_player.rb
+++ computer_player.rb
@@ -9,27 +9,41 @@ class ComputerPlayer
   end
 
   def take_turn
-    start_pos = nil
-    end_pos = nil
     if @board.find_move_into_check
-      move = @board.find_move_into_check
-      start_pos, end_pos = move[0], move[1]
+      put_opponent_in_check
     elsif @board.find_best_capture
-    	move = @board.find_best_capture
-      start_pos, end_pos = move[0], move[1]
+    	make_capture
     else
-      while start_pos.nil? && end_pos.nil?
-        random_piece = @board.current_player_pieces.select { |piece| piece.valid_moves.length > 0 }.sample
-        random_start = random_piece.pos
-        random_end = random_piece.valid_moves.select { |end_pos| !@board.move_into_check?(random_start, end_pos) }.sample
-        if (!random_start.nil? && !random_end.nil?)
-          start_pos = random_start
-          end_pos = random_end
-	      end
-      end
+      make_random_move
     end
-    @board.place_piece(start_pos, end_pos)
     @board.render
   end
 
+  def put_opponent_in_check
+  	move = @board.find_move_into_check
+    start_pos, end_pos = move[0], move[1]
+    @board.place_piece(start_pos, end_pos)
+  end
+
+  def make_capture
+  	move = @board.find_best_capture
+    start_pos, end_pos = move[0], move[1]
+    @board.place_piece(start_pos, end_pos)
+  end
+
+  def make_random_move
+  	start_pos = nil
+    end_pos = nil
+  	while start_pos.nil? && end_pos.nil?
+      random_piece = @board.current_player_pieces.select { |piece| piece.valid_moves.length > 0 }.sample
+      random_start = random_piece.pos
+      random_end = random_piece.valid_moves.select { |end_pos| !@board.move_into_check?(random_start, end_pos) }.sample
+      if (!random_start.nil? && !random_end.nil?)
+        start_pos = random_start
+        end_pos = random_end
+      end
+    end
+    @board.place_piece(start_pos, end_pos)
+  end
+  
 end
